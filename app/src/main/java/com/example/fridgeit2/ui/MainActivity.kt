55881403430403b65a6aef96020e79ed97eb9e50
Clivityy <johnlamsdan@gmail.com>
2023-02-14 16:49:57 +0000

    Replaced Splash screen activity with a fragment instead and removed deprecated Handler function so app does not suddenly stop working.
    Navigation component also been implemented to follow best practises set by Google.
    
    New UI for adding items will be added soon.
    
    As well as notifications to alert user when items are going out of date. Proper date formats should be implemented soon too.

--- app/src/main/java/com/example/fridgeit2/ui/MainActivity.kt
+++ app/src/main/java/com/example/fridgeit2/ui/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.fridgeit2.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import com.example.fridgeit2.R
 
-class SplashScreenActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
-
-        Handler().postDelayed({
-           val intent = Intent(this@SplashScreenActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        },3000)
     }
 }
---
+++ app/src/main/java/com/example/fridgeit2/ui/SplashScreenFragment.kt
@@ -0,0 +1,55 @@
+package com.example.fridgeit2.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import com.example.fridgeit2.R
+import java.util.concurrent.Executor
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class SplashScreenFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val myRunnable = Runnable {
+            findNavController().navigate(R.id.action_splashScreenFragment_to_homeActivity)
+        }
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({ Executor { it.run() }.execute(myRunnable) }, 3000L)
+
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SplashScreenFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
